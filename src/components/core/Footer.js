b0fb5eaca4225beba3f2da95e9bc2dc1c933b8ff
Aaron Sisler <asisler@corp.epnet.com>
2018-06-01 09:05:46 -0400

    Changes from looking through PR

--- src/components/core/Footer.js
+++ src/components/core/Footer.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer>
-        <div>Copyright &copy; {new Date().getFullYear()}</div>
-    </footer>
+    <footer />
 );
 
 export default Footer;
