b525f216640d49f0d15e18052efe2c88b48aeb42
Aaron Sisler <asisler@corp.epnet.com>
2018-11-01 15:35:17 -0400

    Removing add notification given it isn't being used currently

--- src/actions/notifications.js
+++ src/actions/notifications.js
@@ -1,13 +1,6 @@
 import database from 'Firebase/firebase';
 import { setNotifications } from 'Actions/helpers/notifications';
 
-export const startAddNotification = () => (undefined, getState) => {
-    const { uid: userId } = getState().auth;
-    const notification = { hasBeenViewed: false, notificationMessage: 'I am a notification' };
-
-    return database.ref(`notifications/${userId}`).push({ ...notification });
-}
-
 export const startStreamNotifications = () => (dispatch, getState) => {
     const { uid: userId } = getState().auth;
 
