11c7b6bc5cb0e1119e7ea8aa9146931d472fb2a0
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-03-02 22:41:07 -0500

    Helps if you test the right function :-)

--- test/selectors/applicants.test.js
+++ test/selectors/applicants.test.js
@@ -1,27 +1,27 @@
-import selectPositions from 'Selectors/positions';
+import selectApplicants from 'Selectors/applicants';
 import applicants from '../fixtures/applicants';
 
-describe('selectPositions() method', () => {
+describe('selectApplicants()', () => {
     const [applicant] = applicants;
     const { lastName } = applicant.user;
 
     it('should return all applicants watched if no text filter', () => {
-        const result = selectPositions(applicants, {});
+        const result = selectApplicants(applicants, {});
         expect(result).toEqual(applicants);
     })
 
     it('should filter applicants by last name', () => {
-        const result = selectPositions(applicants, { text: lastName });
+        const result = selectApplicants(applicants, { text: lastName });
         expect(result).toEqual([applicants[0]]);
     })
 
     it('should filter applicants by first name', () => {
-        const result = selectPositions(applicants, { text: 'New First Name' });
+        const result = selectApplicants(applicants, { text: 'New First Name' });
         expect(result).toEqual([applicants[1]]);
     })
 
     it('should filter out all applicants if neither first or last name are matched', () => {
-        const result = selectPositions(applicants, { text: 'non matching' });
+        const result = selectApplicants(applicants, { text: 'non matching' });
         expect(result).toEqual([]);
     })
 })
